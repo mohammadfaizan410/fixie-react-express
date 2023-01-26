3991652bda4b01650c716c49c81e10d57e641269
JordanAbdeen <115732404+JordanAbdeen@users.noreply.github.com>
2023-01-26 12:29:26 +0300

    Login Authentication & Session Initialization

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const sessions = require("express-session");
 const app = express();
 const PORT = 4000;
 const cors = require("cors");
@@ -8,12 +9,13 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
-
+var sess;
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect("mongodb://127.0.0.1:27017/fixie_DB", { useNewUrlParser: true });
+
+const twoMinutes = 1000 * 60 * 2;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,21 +27,49 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
+app.use(sessions({
+    secret: 'thisismyfuckingsecretdudefuckoff',
+    saveUninitialized: false,
+    cookie: { maxAge: twoMinutes },
+    resave: false
+}));
+
 app.set("views", __dirname + "/views")
 
 app.get("/", (req, res) => {
-    res.send("hello homepage");
+    sess = req.session;
+    if (sess.user){
+        res.send(sess.user);
+    }
 })
 
-app.post("/api/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ username: username }).then(result => async () => {
+app.post("/api/login", async (req, res) => {
+    
+    sess = req.session;
+
+    const email = req.body.data.email;
+    const password = req.body.data.password;
+    
+    User.findOne({ email: email }).then(result => {
+
         if (result) {
-            const passMatch = await bcrypt.compare(password, result.password);
-            if (passMatch) {
-                res.send("success")
+            bcrypt.compare(password, result.password, (err, compResult) => {
+            if (compResult){
+                sess=req.session;
+                sess.user = {
+                    _id: result._id,
+                    name: result.name,
+                    surname: result.surname,
+                    email: result.email,
+                    worker: result.worker
+                };
+                res.send(sess);
+            } else {
+                res.send(false);
             }
+            })
+        } else {
+            res.send(false);
         }
     })
 });
@@ -69,4 +99,4 @@ app.post("/api/register", (req, res) => {
     })
 })
 
-app.listen(PORT, console.log(`listening on port ${PORT}`));
+app.listen(PORT, console.log(`Server running at port ${PORT}`));
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,29 +2,50 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import "../styles/login.css";
+import { Navigate, useNavigate } from 'react-router-dom';
+
 function Login() {
     
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(loginData)
-  }
-  
+  const navigate = useNavigate();
+
   const handleChange = (e) => {
     setLoginData({
       ...loginData,
       [e.target.name] : e.target.value
     })
   } 
-
+  
   const [loginData, setLoginData] = React.useState({
     email: '',
     password: ''
   });  
   
-  
+  const getResponse = async() => {
+    try {
+      const response = await axios.post(
+        `http://localhost:4000/api/login`,
+        { data: loginData }
+        );
+        return response;
+      } catch (error){
+        console.log(error);
+        return error;
+      }
+    }
+    
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const res = await getResponse();
+        if (res.data){
+          navigate('/');
+        } else {
+          document.getElementsByClassName('status').text = 'Incorrect email or password';
+        }
+    }
 
-  return (
+    return (
       <Form onSubmit={handleSubmit} className="login-container" >
         <h3>Login!</h3>
     <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -45,9 +66,10 @@ function Login() {
     <Button variant="primary" type="submit">
       Login
       </Button>
+      <div className='status'></div>
       <div>
       <Form.Text className="text-muted login-text" >
-        Don't have an account?<Link to="/register">Register!</Link>
+        Don't have an account? <Link to="/register">Register!</Link>
         </Form.Text>
      </div>
  
